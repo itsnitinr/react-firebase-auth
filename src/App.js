d0839943b3377f6fd70429c0d39c04c198c50eae
Nitin Ranganath <nitinranganath@gmail.com>
2020-10-13 00:15:31 +0530

    Build signup component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Signup from "./components/Signup.component";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const App = () => <h1>Hello World</h1>;
+const App = () => (
+  <Container
+    className="d-flex align-items-center justify-content-center"
+    style={{ minHeight: "100vh" }}
+  >
+    <div className="w-100" style={{ maxWidth: "400px" }}>
+      <Signup />
+    </div>
+  </Container>
+);
 
 export default App;
