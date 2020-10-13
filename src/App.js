136221ed6a300a26dde945e7eea486c41f13399c
Nitin Ranganath <nitinranganath@gmail.com>
2020-10-13 13:21:38 +0530

    Add signup functionality to signup component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { AuthProvider } from "./contexts/AuthContext";
 import Signup from "./components/Signup.component";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => (
-  <Container
-    className="d-flex align-items-center justify-content-center"
-    style={{ minHeight: "100vh" }}
-  >
-    <div className="w-100" style={{ maxWidth: "400px" }}>
-      <Signup />
-    </div>
-  </Container>
+  <AuthProvider>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Signup />
+      </div>
+    </Container>
+  </AuthProvider>
 );
 
 export default App;
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -9,17 +9,25 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
 
   function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password);
   }
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => setCurrentUser(user));
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
     return unsubscribe;
   }, []);
 
   const value = { currentUser, signup };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
