a9461cd3e82ee46b110e9a2201dfcdfb3fc67b69
Nitin Ranganath <nitinranganath@gmail.com>
2020-10-13 15:48:48 +0530

    Build dashboard component

--- src/components/Dashboard.component.jsx
+++ src/components/Dashboard.component.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card, Button, Alert } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
-const Dashboard = () => <h1>Dashboard</h1>;
+const Dashboard = () => {
+  const [error, setError] = useState("");
+  const { currentUser } = useAuth();
+
+  function handleLogout() {}
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email: </strong>
+          {currentUser.email}
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+            Update Profile
+          </Link>
+        </Card.Body>
+        <div className="w-100 text-center mt-2">
+          <Button variant="link" onClick={handleLogout}>
+            Log Out
+          </Button>
+        </div>
+      </Card>
+    </>
+  );
+};
 
 export default Dashboard;
